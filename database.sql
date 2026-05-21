ff15523abb5d60a5d02b2d18560c808b13b2917b
Avinash Rane <avinashrane@Avinashs-MacBook-Pro.local>
2026-05-21 16:12:23 +0530

    Updated database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
-CREATE Database student_management; #
+CREATE DATABASE student_management; 
 
-USE student_management; # This command is used to select the database I just created, so that I can create tables and insert data into it.
+USE student_management; #This command is used to select the database I just created, so that I can create tables and insert data into it.
 
 
-Create Table students ( 
+CREATE TABLE students ( 
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100),
     age INT,
